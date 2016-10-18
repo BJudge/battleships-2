ffe01721eb263da1f3f47401c22850e127b7c91a
BartJudge <BartJudge@gmail.com>
2016-10-18 17:25:46 +0100

    spec_helper error

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -3,4 +3,9 @@ feature 'View hit points' do
     sign_in_and_play
     expect(page).to have_content 'Edd: 60HP'
   end
+
+  scenario 'see Player 1 hit points' do
+    sign_in_and_play
+    expect(page).to have_content 'Bart: 60HP'
+  end
 end
