1e2c5d3349876847d848ef2f5e00b79328b0c72c
BartJudge <BartJudge@gmail.com>
2016-10-18 16:18:12 +0100

    adding web helper and player two points

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,9 +1,6 @@
 feature 'Enter names' do
   scenario 'submitting names' do
-    visit('/')
-    fill_in :player_1_name, with: 'Bart'
-    fill_in :player_2_name, with: 'Edd'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Bart vs. Edd'
   end
 end
